2cf21011a63a227ce92c4ec5a123ce44644cd5cc
Elysia Lucas <elysia.lucas@lasp.colorado.edu>
2018-10-02 16:50:00 -0600

    Cleaned up files

--- VEX_Magnetosphere/code_test.py
+++ VEX_Magnetosphere/code_test.py
@@ -1,6 +1,4 @@
 import VEX_Magnetosphere
-import os
-import datetime
 
 def code_test(start_time,end_time):
     #grab any relevant files in date range
@@ -9,7 +7,6 @@ def code_test(start_time,end_time):
     #load data into Pandas DataFrame
     table = VEX_Magnetosphere.vex_load_data(dates_file,disp=False)
     
-    #print(table)
     #trim data to HH:MM:SS time range
     #table = VEX_Magnetosphere.date_vetting(table,start_time,end_time)
     
@@ -19,5 +16,8 @@ def code_test(start_time,end_time):
     #make mag data vs orbit plot data
     #VEX_Magnetosphere.orbit_mag_plot(table)
     
+    #make 3D orbit plot
     VEX_Magnetosphere.plot_3D(table)
+    
+    
 code_test('2011-03-04 00:00:00','2011-03-04 00:00:00')
--- VEX_Magnetosphere/plot_3D.py
+++ VEX_Magnetosphere/plot_3D.py
@@ -1,8 +1,5 @@
-import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib.patches import FancyArrowPatch
 from mpl_toolkits.mplot3d import proj3d
 
 def plot_3D(table):
@@ -19,42 +16,56 @@ def plot_3D(table):
     mag_line_i = []
     
     scale = 300 #km/T
+    n = 100 #vectors
+    
+    #plot VEX orbut
+    ax.plot(table['XSC'],table['YSC'],table['ZSC'])
     
     #linspace the # of vectors wanted
-    for i in np.linspace(0, num_ti, num=100):
+    for i in np.linspace(0, num_ti, num=n):
         mag_line_i = mag_line_i + [int(round(i))]
-        
+
+    #plot each MAG 3D vector individually
     for i in mag_line_i:
-#         print(table['Bx'][i], table['By'][i])
-#         #if positive direction, red
-#         if table['Bz'][i] > 0:
-#             c = 'r'
-#         #if negative direction, blue
-#         else:
-#             c = 'b'
         Bplot = ax.plot([table['XSC'][i],table['XSC'][i]+scale*table['Bx'][i]],
                         [table['YSC'][i],table['YSC'][i]+scale*table['By'][i]],
                         [table['ZSC'][i],table['ZSC'][i]+scale*table['Bz'][i]],
                         color='r')
+        #print(Bplot)
         #fig.colorbar(Bplot)
-    ax.plot(table['XSC'],table['YSC'],table['ZSC'])
+#     print(len(table['ZSC'].values))
+#     print(len(table['ZSC'].values+scale*table['Bx'].values))
+#     Bplot = ax.plot([table['XSC'].values,table['XSC'].values+scale*table['Bx'].values],
+#                         [table['YSC'].values,table['YSC'].values+scale*table['By'].values],
+#                         [table['ZSC'].values,table['ZSC'].values+scale*table['Bz'].values])
+        #fig.colorbar(Bplot)
+#     plt.set_cmap('plasma')
+    
+    #add venus to plot
     add_venus_3D(ax)
+    
+    #scale axes to mostly square
     ax.auto_scale_xyz([-35000, 35000], [-35000, 35000], [-65000, 5000])
     
+    #set labels
     ax.set_xlabel('VSO X')
     ax.set_ylabel('VSO Y')
     ax.set_zlabel('VSO Z')
     
+    #add sun vector
     ax.quiver(0,0,0,35000,0,0,length=1.0,arrow_length_ratio=0.1,color='y')
 
+    #plot title
     plt.title('VEX Orbit MAG Data: '+time_range_str)
     plt.show()
     
 def add_venus_3D(ax):
+    #add wireframe sphere representing venus
     u, v = np.mgrid[0:2*np.pi:20j, 0:np.pi:10j]
     x = np.cos(u)*np.sin(v)
     y = np.sin(u)*np.sin(v)
     z = np.cos(v)
     
-    r = 6051.8
+    r = 6051.8 #km (venus radius)
+    #plot orange wireframe sphere
     ax.plot_wireframe(r*x, r*y, r*z, color=(1,165/255,0))
