af3e807d7821a641782b0b00a72239b9d5de185c
Elysia Lucas <elysia.lucas@lasp.colorado.edu>
2019-03-14 16:56:25 -0600

    Supplementary calls to map_plots for auto plot generation

--- VEX_Magnetosphere/bin_main.py
+++ VEX_Magnetosphere/bin_main.py
@@ -40,12 +40,13 @@ def bin_main(start_time,
     orbit_bin(start_time,end_time,mag='Bz',dim=['XSC','YSC'],ns=ns,append=append,slice=slice,slice2=slice2)
     orbit_bin(start_time,end_time,mag='|B|',dim=['XSC','YSC'],ns=ns,append=append,slice=slice,slice2=slice2)
     
-#bin_main('2014-01-01','2014-03-01',ns=True,append='testBS')
+bin_main('2014-02-24','2014-03-27',slice2=True,append='testorbit2')
+map_plots('2014-02-24','2014-03-27',append="testorbit2")
 #bin_main('2006-04-25','2014-11-24',ns=True,append='full_nn3')
 #bin_main('2010-12-20','2010-12-21',slice2=True,append='orbit2')
 #bin_main('2014-09-14','2014-12-14',slice=True,append='avg025z3')
 #bin_main('2014-09-14','2014-12-14',slice2=True,append='avg025y3')
-dates = pd.date_range('2014-01-05','2014-04-04',freq='D').astype(str).tolist()
-for date in dates:
-    bin_main('2014-01-04',date,slice2=True,append='025y3')
-    map_plots('2014-01-04',date,append="025y3")
+# dates = pd.date_range('2014-03-27','2014-04-04',freq='D').astype(str).tolist()
+# for date in dates:
+#     bin_main('2014-01-04',date,slice2=True,append='025y3')
+#     map_plots('2014-01-04',date,append="025y3")
