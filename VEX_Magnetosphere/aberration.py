7b04496689fb563de64c0bdfb034574ccf2977a2
Elysia Lucas <elysia.lucas@lasp.colorado.edu>
2019-05-01 17:26:44 -0600

    Commented

--- VEX_Magnetosphere/aberration.py
+++ VEX_Magnetosphere/aberration.py
@@ -2,18 +2,21 @@ import pandas as pd
 import numpy as np
 
 def aberration(table):
+    #make copy of VSE table
     ab_table = table.copy()
+    #for each time in table
     for time in table.index:
-        #correct for aberration (6 degrees)
+        #set aberration angle at Venus (6 degrees) in radians
         theta = -0.104719755
         
+        #establish rotation matrix (rotate around z-axis)
         c,s = np.cos(theta), np.sin(theta)
-
         rot_ab = np.array(((c,-s,0),
                            (s,c,0),
                            (0,0,1)))
         
         
+        #rotate magnetic field and location values
         Bx = ab_table['Bx'][time]
         By = ab_table['By'][time]
         Bz = ab_table['Bz'][time]
