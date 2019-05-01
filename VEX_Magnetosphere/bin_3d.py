d62a808b04265d186777a67f3daf792cf7df6dff
Elysia Lucas <elysia.lucas@lasp.colorado.edu>
2019-05-01 17:27:11 -0600

    Commented

--- VEX_Magnetosphere/bin_3d.py
+++ VEX_Magnetosphere/bin_3d.py
@@ -5,18 +5,15 @@ from matplotlib import ticker
 from VEX_Magnetosphere import *
 
 def bin_3d(final_stat,final_x,final_y,dim='x',v_toggle='off',save=False,name=None,title=None,bs='off',magB=False):
-    #initialize colorbar label
-    bindim = r'$B_{' + dim + r'}$ Strength (nT)'
-    print(bindim)
+
     #create figure
     fig,ax = plt.subplots(nrows=1, ncols=1)
     #create colorbar axis
     cax = plt.axes([0.85, 0.1, 0.05, 0.8])
     #adjust main plot to make room for colorbar
     plt.subplots_adjust(bottom=0.1, right=0.8, top=0.9)
+    
     #create edges between -3 and 3 Rv
-#     yedges = np.arange(-3,3,0.1)
-#     zedges = np.arange(-3,3,0.1)
     yedges = np.arange(-3,3.2,0.2)
     zedges = np.arange(-3,3.2,0.2)
     #make meshgrid to plot bins
@@ -26,19 +23,22 @@ def bin_3d(final_stat,final_x,final_y,dim='x',v_toggle='off',save=False,name=Non
     ax.add_artist(venus1)
     #pick r/w/b divergent colormap
     cmap = plt.get_cmap('seismic')
-    #pick bounds for colorbar to be -20 to 20 with 60 distinct colors
+    #pick bounds for colorbar to be -10 to 10 with 60 distinct colors
     bounds = np.linspace(-10, 10, 60)
     #set colorbar norm and other settings
     norm = matplotlib.colors.BoundaryNorm(bounds, cmap.N)
     cb = matplotlib.colorbar.ColorbarBase(cax, cmap=cmap, norm=norm, ticks=bounds, orientation='vertical')
+    
+    #set colorbar label
     if magB==False:
-        cb.set_label(bindim)
+        cb.set_label(r'$B_{' + dim + r'}$ Strength (nT)')
     else:
         cb.set_label(r'$|B|$ Strength (nT)')
+    #set colorbar ticks
     tick_locator = ticker.MaxNLocator(nbins=11)
     cb.locator = tick_locator
     cb.update_ticks()
-    #plot bins
+    #plot bins with -10 to 10 nT range
     ax.pcolormesh(ymesh,zmesh,final_stat,cmap=cmap,vmin=-10, vmax=10)
 
     #set axis limits     
@@ -56,7 +56,7 @@ def bin_3d(final_stat,final_x,final_y,dim='x',v_toggle='off',save=False,name=Non
         ax.set_xlabel('XSC')
         ax.set_ylabel('YSC')
     
-    #toggle 2D venus on/off
+    #toggle 2D black and white venus on/off
     if v_toggle == 'on':
         if dim == 'x':
             add_venus_2D((0,0), 1, 90, ax, colors=('w','w'))
@@ -66,8 +66,6 @@ def bin_3d(final_stat,final_x,final_y,dim='x',v_toggle='off',save=False,name=Non
             add_venus_2D((0,0), 1, 90, ax, colors=('k','w'))
         
     #create mesh to plot vectors in middle of each bin    
-#     yedges = np.arange(-2.95,3.05,0.1)
-#     zedges = np.arange(-2.95,3.05,0.1)
     yedges = np.arange(-2.9,3.3,0.2)
     zedges = np.arange(-2.9,3.3,0.2)
     ymesh,zmesh = np.meshgrid(yedges,zedges)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
@@ -78,10 +76,7 @@ def bin_3d(final_stat,final_x,final_y,dim='x',v_toggle='off',save=False,name=Non
     #add magnetic field direction vectors, autoscale length
     ax.quiver(X, Y, U, V,scale=25)
 
-
-
-
-    #add BS marker
+    #add BS to plots dependent on projection
     if bs=='on':
         L = 1.303
         epsilon = 1.056
@@ -99,6 +94,7 @@ def bin_3d(final_stat,final_x,final_y,dim='x',v_toggle='off',save=False,name=Non
         if dim == 'y' or dim == 'z':
             ax.plot(x,BS,color='k',linestyle=':')
             ax.plot(x,-BS,color='k',linestyle=':')
+            
     #set title, either show/save figure            
     if title != None:
         ax.set_title(title)
