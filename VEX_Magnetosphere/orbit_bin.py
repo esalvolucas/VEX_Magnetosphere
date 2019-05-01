bfd33112d8704c8b2e778ca846587d0b65d9f9cc
Elysia Lucas <elysia.lucas@lasp.colorado.edu>
2019-05-01 17:29:52 -0600

    Commented, pressure selection implemented
    Using lognormal distribution of swpressure:
    low < -0.5stdev
    -0.5stdev < med < 0.5stdev
    0.5stdev < high

--- VEX_Magnetosphere/orbit_bin.py
+++ VEX_Magnetosphere/orbit_bin.py
@@ -5,11 +5,13 @@ import _pickle as cPickle
 from VEX_Magnetosphere.date_to_orbit import *
 from VEX_Magnetosphere.rotate_to_VSE import *
 
-def orbit_bin(start_time,end_time,mag='Bx',dim=['YSC','ZSC'],append=None,slice=None):
+def orbit_bin(start_time,end_time,mag='Bx',dim=['YSC','ZSC'],append=None,pres=None,slice=None):
+    #initialize fail statistics
     table_fail = 0
     rotation_fail = 0
     bin_fail = 0
     
+    #create filenames to write binned data to
     pkl_name = start_time[0:10]+'_'+end_time[0:10]+'_'+mag+'_'+dim[0]+'_'+dim[1]
     if mag == '|B|':
         pkl_name = start_time[0:10]+'_'+end_time[0:10]+'_B_'+dim[0]+'_'+dim[1]
@@ -19,7 +21,8 @@ def orbit_bin(start_time,end_time,mag='Bx',dim=['YSC','ZSC'],append=None,slice=N
         pkl_name = pkl_name + "_" + append
         
     print(pkl_name)
-
+    
+    #create file names for 3D/2D binning structures
     pkl_name3D = "./VEX_data_files/VSE/VEX_bin_" + pkl_name + "_3D.pkl"
     pkl_name2D = "./VEX_data_files/VSE/VEX_bin_" + pkl_name + "_2D.pkl"
     mag = 'VEX.'+mag
@@ -36,79 +39,65 @@ def orbit_bin(start_time,end_time,mag='Bx',dim=['YSC','ZSC'],append=None,slice=N
     start_o = date_to_orbit(start_time)
     end_o = date_to_orbit(end_time)
     orbits = np.arange(start_o,end_o,1)
-    #initialize 60x60 binning matrix
-    #final_stat = np.zeros((60,60))
-    #final_nan = np.zeros((60,60))
+    #initialize 31x31 binning matrix
     final_stat = np.zeros((31,31))
     final_nan = np.zeros((31,31))
+    
+    #read in all solar wind data
+    swdata = read_sw(flag=[0,1])
+    
     #for each orbit
     for orbit in orbits:
         print(orbit)
         try:
             #load data into dataframe
             VSE_table = orbit_load(orbit)
-            #select hour of data outside BS crossings
-            #CA_select_in = table.iloc[BS_in-60:BS_in]
-            #CA_select_out = table.iloc[BS_out:BS_out+60]
+            #append specific orbit of solar wind data to table
+            VSE_table = append_sw(VSE_table,swdata)
+
         except:
             print('table load fail')
             table_fail += 1
             pass
  
-#         try:
-#             #perform coordinate rotation
-#             #ab_table = aberration(table)
-#             VSE_table = rotate_to_VSE(table,BS_in,BS_out,CA_select_in,CA_select_out)
-#             #print(VSE_table)
-#         except:
-#             print('rotation fail')
-#             rotation_fail += 1
-#             pass
-        
-        #try:
-            #choose just nightside data if keyword implemented
+        #pick spatial slice of 3D cube
         if slice is not None:
+            #XSC, YSC, or ZSC
             plane = slice[0]
+            #lower bound
             minus_delta = slice[1]
+            #upper bound
             plus_delta = slice[2]
+            #pick bounded slice out of data
             VSE_table = VSE_table.where((VSE_table[plane]<plus_delta)&(VSE_table[plane]>minus_delta))
-        
+            
+        #sort data by low/med/high solar wind pressure
+        if pres=='low':
+            VSE_table = VSE_table.where((np.log(VSE_table['Pressure'])<=-20.83))
+        elif pres=='med':
+            VSE_table = VSE_table.where((np.log(VSE_table['Pressure'])<-19.58)&(np.log(VSE_table['Pressure'])>-20.83))
+        elif pres=='high':
+            VSE_table = VSE_table.where((np.log(VSE_table['Pressure'])>=-19.58))
+            
         #initialize insitu structre for pydivide.bin
         insitu = {}
         insitu['VEX'] = VSE_table
-        #use pydivide.bin to bin data in 3D, bins of 0.1, between -3 and 3 Rv
-    #             VSE_binavg,VSE_counts = bin(insitu,mag,['VEX.XSC','VEX.YSC','VEX.ZSC'],avg=True,
-    #                                         density=True,binsize=[0.1,0.1,0.1],mins=[-3,-3,-3],
-    #                                         maxs=[3,3,3])
+        #use pydivide.bin to bin data in 3D, bins of 0.2, between -3 and 3 Rv
         VSE_binavg,VSE_counts = bin(insitu,mag,['VEX.XSC','VEX.YSC','VEX.ZSC'],avg=True,
                                     density=True,binsize=[0.2,0.2,0.2],mins=[-3,-3,-3],
                                     maxs=[3.2,3.2,3.2])
         VSE_binavg = VSE_binavg*VSE_counts
-        #print((np.isnan(VSE_binavg)).all())
 
-        #print(VSE_binavg)
-        #np.set_printoptions(threshold=np.nan)
-        #take mean of binned data on axis to collapse
+        #take sum of binned data on collapse axis
         xy_arr = np.nansum(VSE_binavg,axis=collapse)
-        #print(xy_arr)
-        #print(xy_arr[np.where(xy_arr<0)])
-        #print(xy_arr)
-        #counts bases binning off of data points/bin/orbit, instead of logical 1 or 0 for (data or no data)/orbit
-
+        #take sum of # of data points on collapse axis
         xy_nan = np.nansum(VSE_counts,axis=collapse)
+        #if nan, make 0
         xy_nan[np.isnan(xy_nan)] = 0
         #add data to final arrays
         final_stat = np.nansum(np.dstack((final_stat,xy_arr)),2)
-        #print(final_stat)
-        #final_nan = 
-        #final_stat += xy_arr
-        #print(final_stat)
         final_nan += xy_nan
-        
-        #except:
-        #    print('bin fail')  
-        #    bin_fail += 1
-        #    continue
+
     #replace any instances of 0 with 1 to not break np.divide
     final_nan[np.where(final_nan==0)] = 1
     #divide final data by final number of points per bin
@@ -128,6 +117,7 @@ def orbit_bin(start_time,end_time,mag='Bx',dim=['YSC','ZSC'],append=None,slice=N
     output3D.close()
     output2D.close()
     
+    #print bin statistics to more easily diagnose errors
     print('******* ORBIT BIN STATISTICS *******')
     print('Table Failures:',100*table_fail/len(orbits),'%')
     print('Rotation Failures:',100*rotation_fail/len(orbits),'%')
