7e075216f76d1c3a3c3fe399193d80692f5f93d2
Elysia Lucas <elysia.lucas@lasp.colorado.edu>
2019-05-01 17:31:21 -0600

    Commented
    Extraneous statements removed

--- VEX_Magnetosphere/orbit_load.py
+++ VEX_Magnetosphere/orbit_load.py
@@ -2,19 +2,17 @@ import pandas as pd
 import glob
 
 def orbit_load(orbit_num):
-    #remove column wrapping output
+    #establish 4-digit orbit number
     orbit_num = str(orbit_num).zfill(4)
+    #prevent dataframe output wrapping
     pd.set_option('display.width', None)
     #open file, read into pandas dataframe
     path = "./VEX_data_files/VSE/ORBIT_"+orbit_num+"_BS_*"
     for filename in glob.glob(path):
         print(filename)
-        #BS_in = int(filename[31:34])
-        #BS_out = int(filename[35:38])
         with open(filename) as f:
             table = pd.read_csv(f,sep='\t',index_col=0,header=None,names = ['UTC','Bx','By','Bz','|B|','XSC','YSC','ZSC','RSC','Clock','Cone','BS-rho'])
             #set UTC column to datetimeindex
             table = table.set_index(pd.DatetimeIndex(table.index))
         f.close()
     return table
-#print(orbit_load(2932))
