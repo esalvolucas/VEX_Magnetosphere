d345ad1f5b2563748b73ac0c1d2b11f7d121f4ff
Elysia Lucas <elysia.lucas@lasp.colorado.edu>
2019-05-24 10:04:40 -0600

    Add in more Z slices

--- VEX_Magnetosphere/plotgen.py
+++ VEX_Magnetosphere/plotgen.py
@@ -25,6 +25,28 @@ def plotgen(startdate,enddate,pres=None,v=None,E=None):
         bin_main(startdate,enddate,slice=['ZSC',-0.25,0.25],append='ZSChigh',pres='high')
         map_plots(startdate,enddate,append='ZSChigh',slicedir='ZSC',pres=pres)
         sleep(7)
+        
+        #1.0 < ZSC < 1.5
+        bin_main(startdate,enddate,slice=['ZSC',1.0,1.5],append='ZSC115low',pres='low')
+        map_plots(startdate,enddate,append='ZSC115low',slicedir='ZSC',pres=pres)
+        sleep(7)
+        bin_main(startdate,enddate,slice=['ZSC',1.0,1.5],append='ZSC115med',pres='med')
+        map_plots(startdate,enddate,append='ZSC115med',slicedir='ZSC',pres=pres)
+        sleep(7)
+        bin_main(startdate,enddate,slice=['ZSC',1.0,1.5],append='ZSC115high',pres='high')
+        map_plots(startdate,enddate,append='ZSC115high',slicedir='ZSC',pres=pres)
+        sleep(7)
+        
+        #-1.5 < ZSC < -1.0
+        bin_main(startdate,enddate,slice=['ZSC',-1.5,-1.0],append='ZSC-115low',pres='low')
+        map_plots(startdate,enddate,append='ZSC-115low',slicedir='ZSC',pres=pres)
+        sleep(7)
+        bin_main(startdate,enddate,slice=['ZSC',-1.5,-1.0],append='ZSC-115med',pres='med')
+        map_plots(startdate,enddate,append='ZSC-115med',slicedir='ZSC',pres=pres)
+        sleep(7)
+        bin_main(startdate,enddate,slice=['ZSC',-1.5,-1.0],append='ZSC-115high',pres='high')
+        map_plots(startdate,enddate,append='ZSC-115high',slicedir='ZSC',pres=pres)
+        sleep(7)
          
         #0.75 < XSC < 1.25
         bin_main(startdate,enddate,slice=['XSC',0.75,1.25],append='XSClow',pres='low')
@@ -103,7 +125,30 @@ def plotgen(startdate,enddate,pres=None,v=None,E=None):
         bin_main(startdate,enddate,slice=['ZSC',-0.25,0.25],append='ZSChigh',v='high')
         map_plots(startdate,enddate,append='ZSChigh',slicedir='ZSC',v=v)
         sleep(7)
-            
+        
+        #1.0 < ZSC < 1.5
+        bin_main(startdate,enddate,slice=['ZSC',1.0,1.5],append='ZSC115low',v='low')
+        map_plots(startdate,enddate,append='ZSC115low',slicedir='ZSC',v=v)
+        sleep(7)
+        bin_main(startdate,enddate,slice=['ZSC',1.0,1.5],append='ZSC115med',v='med')
+        map_plots(startdate,enddate,append='ZSC115med',slicedir='ZSC',v=v)
+        sleep(7)
+        bin_main(startdate,enddate,slice=['ZSC',1.0,1.5],append='ZSC115high',v='high')
+        map_plots(startdate,enddate,append='ZSC115high',slicedir='ZSC',v=v)
+        sleep(7)
+        
+        #-1.5 < ZSC < -1.0
+        bin_main(startdate,enddate,slice=['ZSC',-1.5,-1.0],append='ZSC-115low',v='low')
+        map_plots(startdate,enddate,append='ZSC-115low',slicedir='ZSC',v=v)
+        sleep(7)
+        bin_main(startdate,enddate,slice=['ZSC',-1.5,-1.0],append='ZSC-115med',v='med')
+        map_plots(startdate,enddate,append='ZSC-115med',slicedir='ZSC',v=v)
+        sleep(7)
+        bin_main(startdate,enddate,slice=['ZSC',-1.5,-1.0],append='ZSC-115high',v='high')
+        map_plots(startdate,enddate,append='ZSC-115high',slicedir='ZSC',v=v)
+        sleep(7)
+        bin_main(startdate,enddate,slice=['ZSC',1.0,1.5],append='ZSC115med',v='med')    
+        
         #0.75 < XSC < 1.25
         bin_main(startdate,enddate,slice=['XSC',0.75,1.25],append='XSClow',v='low')
         map_plots(startdate,enddate,append='XSClow',slicedir='XSC',v=v)
@@ -159,35 +204,46 @@ def plotgen(startdate,enddate,pres=None,v=None,E=None):
         map_plots(startdate,enddate,append='allhigh',v=v)
         
     if pres == None and v == None and E == None:
-        #-0.25 < YSC < 0.25
+        pass
+        #0.25 < YSC < 0.25
         bin_main(startdate,enddate,slice=['YSC',-0.25,0.25],append='YSCall')
         map_plots(startdate,enddate,append='YSCall',slicedir='YSC')
-          
-        #-0.25 < ZSC < 0.25
+           
+        #0.25 < ZSC < 0.25
         bin_main(startdate,enddate,slice=['ZSC',-0.25,0.25],append='ZSCall')
         map_plots(startdate,enddate,append='ZSCall',slicedir='ZSC')
         sleep(7)
-        
+         
         #0.75 < XSC < 1.25
         bin_main(startdate,enddate,slice=['XSC',0.75,1.25],append='XSCall')
         map_plots(startdate,enddate,append='XSCall',slicedir='XSC')
         sleep(7)
-          
-        #-2.0 < XSC < -1.0
+           
+        #2.0 < XSC < -1.0
         bin_main(startdate,enddate,slice=['XSC',-2.0,-1.0],append='XSCNSall')
         map_plots(startdate,enddate,append='XSCNSall',slicedir='XSC')
         sleep(7)
-            
+             
         #0.5 < XSC < 1.0
         bin_main(startdate,enddate,slice=['XSC',0.5,1.0],append='XSC0510all')
         map_plots(startdate,enddate,append='XSC0510all',slicedir='XSC')
         sleep(7)
-              
+               
         #0.0 < XSC < 0.5
         bin_main(startdate,enddate,slice=['XSC',0.0,0.5],append='XSC0005all')
         map_plots(startdate,enddate,append='XSC0005all',slicedir='XSC')
         sleep(7)
-
+        
+        #1.0 < ZSC < 1.5
+        bin_main(startdate,enddate,slice=['ZSC',1.0,1.5],append='ZSC115low',pres='low')
+        map_plots(startdate,enddate,append='ZSC115low',slicedir='ZSC',pres=pres)
+        sleep(7)
+        
+        #-1.5 < ZSC < 1.0
+        bin_main(startdate,enddate,slice=['ZSC',-1.5,-1.0],append='ZSC-115low',pres='low')
+        map_plots(startdate,enddate,append='ZSC-115low',slicedir='ZSC',pres=pres)
+        sleep(7)
+        
         #All
         bin_main(startdate,enddate,slice=None,append='all')
         map_plots(startdate,enddate,append='all')
